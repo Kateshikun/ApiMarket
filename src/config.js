006324f78030fe52ee2b865bbbaf07f2808d8a15
Kateshikun <danieladriancastillogarcia@gmail.com>
2026-05-07 07:38:22 -0600

    Cambio de variables para la BD

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 import{config} from 'dotenv'
 config()
 
-export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || 'localhost'
-export const DB_PORT=process.env.DB_PORT || 3306
-export const DB_USER=process.env.DB_USER || 'root'
-export const DB_PASSWORD=process.env.DB_PASSWORD || ''
-export const DB_DATABASE=process.env.DB_DATABASE || 'market'
+export const MYSQL_DATABASE="railway"
+export const MYSQL_PUBLIC_URL="mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}/${{MYSQL_DATABASE}}"
+export const MYSQL_ROOT_PASSWORD="wMiuGpYKDGDDTuVZsMTJPIoXBdZfejOo"
+export const MYSQL_URL="mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}"
+export const MYSQLDATABASE="${{MYSQL_DATABASE}}"
+export const MYSQLHOST="${{RAILWAY_PRIVATE_DOMAIN}}"
+export const MYSQLPASSWORD="${{MYSQL_ROOT_PASSWORD}}"
+export const MYSQLPORT="3306"
+export const MYSQLUSER="root"
